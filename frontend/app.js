0630511f6d6aa1063ca5c98feeb274348ccdd056
enderflop <agbarloon@uiowa.edu>
2024-03-07 18:30:42 -0600

    stats showing up in buttons

--- frontend/app.js
+++ frontend/app.js
@@ -17,13 +17,11 @@ document.addEventListener('DOMContentLoaded', () => {
   // fine tune point values
   // make look good
 
-  //https://develop.battle.net/documentation/guides/getting-started
-  //https://develop.battle.net/documentation/hearthstone/game-data-apis
-
   // code here
 
   class Card {
-    constructor(name, cardType, manaCost, attack, health, rarity, cardClass, set, flavorText, keywords, spellSchool, durability, armor) {
+    constructor(id, name, cardType, manaCost, attack, health, rarity, cardClass, set, flavorText, keywords, spellSchool, durability, armor) {
+      this.id = id
       this.name = name
       this.cardType = cardType
       this.manaCost = manaCost
@@ -48,6 +46,7 @@ document.addEventListener('DOMContentLoaded', () => {
     let cards = []
     data.forEach(element => {
       cards.push(new Card(
+        element.id,
         element.name,
         element.type,
         element.cost,
@@ -61,11 +60,60 @@ document.addEventListener('DOMContentLoaded', () => {
         element.spellSchool,
         element.durability,
         element.armor
+
       ))
     });
     console.log(cards)
+
+    displayCard(cards)
   })
 
-  
+  function displayCard(cards){
+    let randomCard = cards[Math.floor(Math.random() * cards.length)]
+
+    let cardDiv = document.getElementById('card-div');
+    cardDiv.innerHTML = '';
+
+    let cardType = document.createElement('h1')
+    cardType.innerText = randomCard.cardType
+    cardDiv.appendChild(cardType)
+
+    createButton("card-name", "Card Name", randomCard.name);
+    createButton("mana-cost", "Mana Cost", randomCard.manaCost);
+    createButton("attack", "Attack", randomCard.attack);
+    createButton("health", "Health", randomCard.health);
+    createButton("rarity", "Rarity", randomCard.rarity);
+    createButton("card-class", "Card Class", randomCard.cardClass);
+    createButton("set", "Set", randomCard.set);
+    createButton("flavor-text", "Flavor Text", randomCard.flavorText);
+    createButton("keywords", "Keywords", randomCard.keywords);
+    createButton("spell-school", "Spell School", randomCard.spellSchool);
+    createButton("durability", "Durability", randomCard.durability);
+    createButton("armor", "Armor", randomCard.armor);
+    createButton("card-art", "Card Art", getCardArt(randomCard.id));
+
+    function createButton(id, text, property) {
+      if (property == undefined) {return}
+      let button = document.createElement('button');
+      button.id = id;
+      button.innerText = text;
+      button.onclick = () => {
+      button.innerText = property;
+      }
+      cardDiv.appendChild(button);
+    }
+
+    function getCardArt(cardId) {
+      return fetch(`https://art.hearthstonejson.com/v1/orig/${cardId}.png`, { mode: 'no-cors' })
+        .then(response => response.blob())
+        .then(blob => {
+          const imgUrl = URL.createObjectURL(blob);
+          const img = document.createElement('img');
+          img.src = imgUrl;
+          return img;
+        });
+    }
+
+  }
 
 })
